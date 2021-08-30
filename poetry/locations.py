8c52b84604cfa807c3aaae00bad181b703f6a707
bennyl <bennyl@voyagerlabs.co>
2021-08-30 14:39:45 +0300

    self update fix

--- poetry/locations.py
+++ poetry/locations.py
@@ -8,14 +8,14 @@ from .utils.appdirs import user_data_dir
 
 
 CACHE_DIR = user_cache_dir("pypoetry")
-DATA_DIR = user_data_dir("pypoetry")
+DATA_DIR = user_data_dir("relaxed-poetry")
 CONFIG_DIR = user_config_dir("pypoetry")
 
 REPOSITORY_CACHE_DIR = Path(CACHE_DIR) / "cache" / "repositories"
 
 
 def data_dir() -> Path:
-    if os.getenv("POETRY_HOME"):
-        return Path(os.getenv("POETRY_HOME")).expanduser()
+    if os.getenv("RP_HOME"):
+        return Path(os.getenv("RP_HOME")).expanduser()
 
-    return Path(user_data_dir("pypoetry", roaming=True))
+    return Path(user_data_dir("relaxed-poetry", roaming=True))
